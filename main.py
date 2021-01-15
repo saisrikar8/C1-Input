30a5b3956bdabdb82796a7f26f9ac8cad1a135d9
Tummala Sai <110589@eesd.org>
2021-01-15 00:27:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+'''''
+01/14/2021
+C1; Input
+
+Review:
+How do you get Output??
+>>print("Hello World")
+OUTPUT: HELLO WORLD
+
+Input:
+The way the computer gets information from the user
+
+Python initially treats inputs as a string type
+
+Ex.
+5 --> "5"
+hi --> "hi"
+
+Formula creating a variable
+  variableName = Data/Value
+
+Formula for creating an input variable:
+  variableName = input(Prompt)
+
+'''
+
+name = "Sai"
+print("My name is " + name)
+
+name1 = input("\nWhat is your name?:\n")
+
+
+
+#Ask user for school
+school = input("What school do you attend?:\n")
+
+
+#Ask user for grade
+grade = input("What grade are you in?:\n")
+
+
+#Ask user for favorite color
+favColor = input("What is you favorite color?:\n")
+
+
+#Ask user for the year/month they were born
+year_Month = input("What year and month were you born in?\n")
+
+
+#Ask user for age
+
+age = input("How old are you?:\n")
+#Print These All Out
+print("\nYour name is " + name1 +"\n You attend " + school + "\n You are in the " + grade+ "\n Your favorite color is " + favColor + "\n You were born in " + year_Month + "\n You are " + age)
